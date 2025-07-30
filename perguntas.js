41f17a1cbfd15efa83bf2b12fa9449ae6b19a211
kauanramos129 <andrade.ramos.kauan@escola.pr.gov.br>
2025-07-30 14:21:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Futebol',
+    'Qual é o objetivo principal de uma partida de futebol?',
+    'Marcar mais gols que o time adversário.'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos jogadores cada time tem em campo durante uma partida?',
+    'Cada time tem 11 jogadores em campo.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um cartão vermelho?',
+    'Um cartão vermelho é mostrado ao jogador que comete uma falta grave, resultando em sua expulsão do jogo.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um gol contra?',
+    'Um gol marcado por um jogador em sua própria meta, resultando em um ponto para o time adversário.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a duração de uma partida de futebol profissional?',
+    'Uma partida de futebol profissional tem duas partes de 45 minutos cada, totalizando 90 minutos, sem contar o tempo adicional.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um pênalti?',
+    'Um pênalti é uma cobrança feita a partir da marca de 11 metros, concedida quando uma falta é cometida dentro da área penal.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o nome do torneio internacional de seleções mais importante?',
+    'A Copa do Mundo da FIFA.'
+)
+
+criaCartao(
+    'Futebol',
+    'O que é um drible?',
+    'Um drible é uma habilidade utilizada para passar por um defensor, mantendo a posse da bola.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é a função principal de um goleiro?',
+    'Impedir que a bola entre no gol, defendendo os chutes dos jogadores adversários.'
+)
